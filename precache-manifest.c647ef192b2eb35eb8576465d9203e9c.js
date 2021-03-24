0623369e1edf3130047dbc1963c7379321272ec2
atpking <atpking@gmail.com>
2021-03-24 10:22:21 +0800

    Updates

---
+++ precache-manifest.c647ef192b2eb35eb8576465d9203e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d9659e0cb83053d8796e6d372d451d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3295b5ab5be8630a3364",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ac6e4905c0042f6824d9",
+    "url": "./static/js/2.c2e17bb0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c2e17bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3295b5ab5be8630a3364",
+    "url": "./static/js/main.85dea9bd.chunk.js"
+  },
+  {
+    "revision": "85c00f95c3638e93c5aa",
+    "url": "./static/js/runtime-main.eddc67f6.js"
+  }
+]);
